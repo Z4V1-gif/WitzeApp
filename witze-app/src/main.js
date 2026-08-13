c99576b9e12d098b3c02d4e3a74c50885ceae569
Marius <darksession22@gmail.com>
2026-08-13 13:12:19 +0200

    Fetch js added and fetch function done

--- witze-app/src/main.js
+++ witze-app/src/main.js
@@ -1,1 +1,8 @@
+import { fetchJoke } from '../../fetch';
 import '../styles/style.scss';
+
+
+
+
+const jokeTextElement = document.querySelector('.current-joke__text');
+const newJokeButton = document.querySelector('.current-joke__new');
